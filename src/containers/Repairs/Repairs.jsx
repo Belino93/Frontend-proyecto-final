1647d52d4f1c7da08e81e651482da182181e49a3
belino93 <abelmadrid93@gmail.com>
2022-12-25 19:01:51 +0100

    Create repairs component

---
+++ src/containers/Repairs/Repairs.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Repairs.css'
+
+function Repairs() {
+  return (
+    <div className='container-repairs'>Estas en reparaciones</div>
+  )
+}
+
+export default Repairs
