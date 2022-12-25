ce21ebf0044ae8f341797418fe923943ea78dfd0
belino93 <abelmadrid93@gmail.com>
2022-12-25 18:16:17 +0100

    create home component

---
+++ src/containers/Home/Home.jsx
@@ -0,0 +1,15 @@
+import './Home.css';
+
+import React from 'react'
+
+function Home() {
+
+
+  return (
+    <div className='container-home'>
+        Estas en HOME
+    </div>
+  )
+}
+
+export default Home
