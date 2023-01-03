49a98904d188f1438e8a4aed5988aea448589015
belino93 <abelmadrid93@gmail.com>
2023-01-03 20:08:40 +0100

    expand navbar changed to md

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,24 +3,25 @@ import { useNavigate } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import './Header.css'
 
 function Header() {
 
   const navigate = useNavigate();
 
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
       <Container>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link onClick={() => navigate('/')}>Home</Nav.Link>
-            <Nav.Link onClick={() => navigate('/repairs')}>Repairs</Nav.Link>
+            <Nav.Link onClick={() => navigate('/')}><span className="nav-text">Home</span></Nav.Link>
+            <Nav.Link onClick={() => navigate('/repairs')}><span className="nav-text">Repairs</span></Nav.Link>
           </Nav>
           <Nav>
-            <Nav.Link onClick={() => navigate('/login')}>Login</Nav.Link>
+            <Nav.Link onClick={() => navigate('/login')}><span className="nav-text">Login</span></Nav.Link>
             <Nav.Link onClick={() => navigate('/register')}>
-              Register
+            <span className="nav-text">Register</span>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
