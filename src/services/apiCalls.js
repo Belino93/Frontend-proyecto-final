32a9aac78142052ae0617f3464cd16910a3d21b3
belino93 <abelmadrid93@gmail.com>
2023-01-04 20:43:50 +0100

    update loginUser function, now return token and user data

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -1,17 +1,26 @@
 import axios from "axios";
-const baseUrl = 'http://127.0.0.1:8000/api/'
+const baseUrl = "http://127.0.0.1:8000/api/";
 
 export const getBrands = async () => {
-    const brands = await axios.get(baseUrl + 'devices/brand')
+  const brands = await axios.get(baseUrl + "devices/brand");
 
-    return brands
-}
+  return brands;
+};
 
 export const loginUser = async (user) => {
-    const loginData = await axios.post(baseUrl + "login", {
-        email: user.email,
-        password:user.password,
-    });
-    return loginData
-
-}
+  const loginData = await axios.post(baseUrl + "login", {
+    email: user.email,
+    password: user.password,
+  });
+  if (loginData.data.token !== undefined) {
+    let config = {
+      headers: { Authorization: "Bearer " + loginData.data.token },
+    };
+    const userData = await axios.get(baseUrl + "profile", config);
+    return { 
+        token: loginData.data.token,
+        user:userData.data
+     };
+  }
+  return loginData;
+};
