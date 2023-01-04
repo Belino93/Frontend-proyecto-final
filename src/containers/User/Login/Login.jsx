00be959209d5d36da5c3e67eb8e97d4427f25251
belino93 <abelmadrid93@gmail.com>
2023-01-04 18:41:45 +0100

    Create login component with states

---
+++ src/containers/User/Login/Login.jsx
@@ -0,0 +1,71 @@
+import { useState, useEffect } from "react";
+import "./Login.css"
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import {loginUser} from "../../../services/apiCalls"
+
+function Login() {
+
+    const [user, setUser] = useState({
+        email:"",
+        password:"",
+    });
+
+    const [error, setError] = useState({
+        error:"",
+    })
+
+    const inpHandler = (e) => {
+        setUser((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        loginUser(user).then((res) => {console.log(res.data)}).catch((error) => console.log(error));
+        
+    }
+
+
+  return (
+    <Container fluid className="main min-vh-100">
+      <Row className="container-register">
+        <Col className="d-flex flex-column justify-content-center">
+          <div className="card">
+            <h1 className="register-text">Login</h1>
+            <form className="d-flex flex-column form" onSubmit={handleSubmit}>
+              <label className="inp" htmlFor="inp">
+                <input
+                  type="text"
+                  name="email"
+                  onChange={(e) => inpHandler(e)}
+                />
+                <span className="label">Email</span>
+                <span className="focus-bg"></span>
+              </label>
+              <label className="inp" htmlFor="inp">
+                <input
+                  type="password"
+                  name="password"
+                  onChange={(e) => inpHandler(e)}
+                />
+                <span className="label">Password</span>
+                <span className="focus-bg"></span>
+              </label>
+              <div>
+                <button className="custom-btn btn-1" type="submit">
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Login;
