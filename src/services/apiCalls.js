ff4d4f20da036ab82c15a4e85fd8101454823858
belino93 <abelmadrid93@gmail.com>
2023-01-07 20:20:04 +0100

    getUsersRepairs function added

--- src/services/apiCalls.js
+++ src/services/apiCalls.js
@@ -1,12 +1,11 @@
 import axios from "axios";
 const baseUrl = "http://127.0.0.1:8000/api/";
-const token = localStorage.getItem('token')
+const token = localStorage.getItem("token");
 const config = {
   headers: { Authorization: "Bearer " + token },
 };
 
-export const getBrands = async (userToken) => {
-  
+export const getBrands = async () => {
   const brands = await axios.get(baseUrl + "devices/brand", config);
 
   return brands;
@@ -22,26 +21,35 @@ export const loginUser = async (user) => {
       headers: { Authorization: "Bearer " + loginData.data.token },
     };
     const userData = await axios.get(baseUrl + "profile", config);
-    return { 
-        token: loginData.data.token,
-        user:userData.data
-     };
+    return {
+      token: loginData.data.token,
+      user: userData.data,
+    };
   }
   return loginData;
 };
 
 export const registerUser = async (user) => {
-  const registerData = await axios.post(baseUrl + 'register', {
-    name:user.name,
-    surname:user.surname,
-    email:user.email,
-    password:user.password,
-  })
+  const registerData = await axios.post(baseUrl + "register", {
+    name: user.name,
+    surname: user.surname,
+    email: user.email,
+    password: user.password,
+  });
 
-  return registerData
-}
+  return registerData;
+};
 
-export const getUserRepairs = async(userToken) => {
-  const userRepairs = await axios.get(baseUrl + 'user/repairs', config)
-  return userRepairs
-}
+export const getUserRepairs = async () => {
+  const userRepairs = await axios.get(baseUrl + "user/repairs", config);
+  return userRepairs;
+};
+
+export const getAllRepairs = async () => {
+  const repairs = await axios.get(baseUrl + "repairs", config);
+  return repairs;
+};
+export const getAllUsersRepairs = async () => {
+  const userRepairs = await axios.get(baseUrl + "admin/repairs", config);
+  return userRepairs;
+};
