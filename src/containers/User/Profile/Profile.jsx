94d2d1da799ef6ea3af2a9b83edd8ddaaecfe366
belino93 <abelmadrid93@gmail.com>
2023-01-08 23:32:41 +0100

    add search input component and create input state

--- src/containers/User/Profile/Profile.jsx
+++ src/containers/User/Profile/Profile.jsx
@@ -4,10 +4,12 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { getUserRepairs } from "../../../services/apiCalls";
 import Spinner from "react-bootstrap/Spinner";
+import SearchInput from "../../../components/SearchInput/SearchInput";
 
 function Profile() {
   const user = JSON.parse(localStorage.getItem("user"));
   const [userRepairs, setUserRepairs] = useState([]);
+  const [search, setSearch] = useState('')
 
   useEffect(() => {
     getUserRepairs()
@@ -17,33 +19,57 @@ function Profile() {
       .catch((error) => console.log(error));
   }, []);
 
+  const inputHandler = (search) => {
+    setSearch(search);
+    console.log(search);
+  }
+
+
+
   return (
-    <Container fluid className="d-flex min-vh-100 justify-content-center flex-column text-center">
+    <Container
+      fluid
+      className="d-flex min-vh-100 justify-content-center flex-column text-center container-home gap-2"
+    >
       {userRepairs.length === 0 && (
-        <Row className="container-home flex-grow-1">
+        <Row className="flex-grow-1 container-home">
           <Col>
             <div>
-            <Spinner animation="border" variant='light' className="spinner-load" />
+              <Spinner
+                animation="border"
+                variant="light"
+                className="spinner-load"
+              />
             </div>
           </Col>
         </Row>
       )}
 
       {userRepairs.length > 0 && (
-        <Row className="container-home flex-grow-1">
-            {userRepairs.map((repair, index) => {
-              return (
-                <Col key={index} className="card col-12 col-md-6">
-                  <h1>
-                    {repair.brand} {repair.model}
-                  </h1>
-                  <h3>Repair type: {repair.type}</h3>
-                  <p>IMEI: {repair.imei}</p>
-                  <p>Status: {repair.name}</p>
-                </Col>
-              );
-            })}
+      <>
+      <Row className="">
+        <Col className="mt-1">
+        <SearchInput handler={inputHandler}/>
+        </Col>
+      </Row>
+        <Row className="flex-grow-1">
+          {userRepairs.map((repair, index) => {
+            return (
+              <Col key={index} className="card col-12 col-md-6">
+                <p>No.{repair.id}</p>
+                <p>
+                  {repair.brand} {repair.model}
+                </p>
+                <p>Repair type: <span className="highlighted">{repair.type}</span></p>
+                <p>IMEI: {repair.imei}</p>
+                <p>Created: {repair.created_at.slice(0,10)}</p>
+                <p>Status: <span className="highlighted">{repair.name}</span></p>
+                <p>Last status update: {repair.updated_at?.slice(0, 10)}</p>
+              </Col>
+            );
+          })}
         </Row>
+        </>
       )}
     </Container>
   );
