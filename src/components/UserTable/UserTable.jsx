06e1bb1de784a397aeb02d27cc251e71ffb0c9be
belino93 <abelmadrid93@gmail.com>
2023-01-14 13:08:19 +0100

    fix: auto render

--- src/components/UserTable/UserTable.jsx
+++ src/components/UserTable/UserTable.jsx
@@ -6,7 +6,6 @@ import { deleteUserByAdmin, upgradeUserToAdmin } from "../../services/apiCalls";
 import Pagination from "react-bootstrap/Pagination";
 import Spinner from "react-bootstrap/Spinner";
 
-
 function UserTable({ users, refresh }) {
   const [clickedUser, setClickedUser] = useState({});
   const [show, setShow] = useState(false);
@@ -17,12 +16,12 @@ function UserTable({ users, refresh }) {
   const [currentPage, setCurrentPage] = useState(1);
   const indexOfLastUser = currentPage * usersInPage;
   const indexOfFirstUser = indexOfLastUser - usersInPage;
-  let arrayPage = []
+  let arrayPage = [];
 
   useEffect(() => {
-    arrayPage = users.slice(indexOfFirstUser, indexOfLastUser)
-    setusersArray(arrayPage)
-  }, [currentPage, users])
+    arrayPage = users.slice(indexOfFirstUser, indexOfLastUser);
+    setusersArray(arrayPage);
+  }, [currentPage, users]);
 
   const clickHandler = (user) => {
     setClickedUser(user);
@@ -51,81 +50,94 @@ function UserTable({ users, refresh }) {
   };
 
   const changePage = (e) => {
-    setCurrentPage(e)
-    arrayPage = users.slice(indexOfFirstUser, indexOfLastUser)
-    setusersArray(arrayPage)
-  }
+    setCurrentPage(e);
+    arrayPage = users.slice(indexOfFirstUser, indexOfLastUser);
+    setusersArray(arrayPage);
+  };
 
   let items = [];
   for (let number = 1; number <= 5; number++) {
     items.push(
-      <Pagination.Item key={number} onClick={() => {changePage(number)}}>
+      <Pagination.Item
+        key={number}
+        onClick={() => {
+          changePage(number);
+        }}
+      >
         {number}
       </Pagination.Item>
     );
   }
 
   return (
-    <>
-      <Table bordered hover responsive>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Name</th>
-            <th>Surname</th>
-            <th>Email</th>
-            <th>Role ID</th>
-          </tr>
-        </thead>
-        <tbody>
-          {usersArray.map((user, index) => {
-            return (
-              <tr key={index} onClick={() => clickHandler(user)}>
-                <td>{user?.id}</td>
-                <td>{user?.name}</td>
-                <td>{user?.surname}</td>
-                <td>{user?.email}</td>
-                <td>{user?.role_id}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </Table>
-      <div className="d-flex align-items-center justify-content-center">
-        <Pagination>{items}</Pagination>
-        <br />
+    <>{usersArray.length === 0 && (
+      <div>
+        <Spinner animation="border" variant="dark" className="spinner-load" />
       </div>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          <Modal.Title>
-            <p>User: {clickedUser?.id}</p>
-            <p>
-              {clickedUser?.name} {clickedUser?.surname}
-            </p>
-            <p>{clickedUser?.email}</p>
-          </Modal.Title>
-        </Modal.Header>
-        {clickedUser?.role_id === 1 && (
-          <Modal.Footer className=" justify-content-center">
-            <Button
-              variant="danger"
-              onClick={() => {
-                deleteButtonHandler(clickedUser);
-              }}
-            >
-              Delete
-            </Button>
-            <Button
-              variant="primary"
-              onClick={() => {
-                upgradeButtonHandler(clickedUser);
-              }}
-            >
-              Upgrade
-            </Button>
-          </Modal.Footer>
-        )}
-      </Modal>
+    )}
+      {usersArray.length > 0 && (
+        <>
+          <Table bordered hover responsive>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Surname</th>
+                <th>Email</th>
+                <th>Role ID</th>
+              </tr>
+            </thead>
+            <tbody>
+              {usersArray.map((user, index) => {
+                return (
+                  <tr key={index} onClick={() => clickHandler(user)}>
+                    <td>{user?.id}</td>
+                    <td>{user?.name}</td>
+                    <td>{user?.surname}</td>
+                    <td>{user?.email}</td>
+                    <td>{user?.role_id}</td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+          <div className="d-flex align-items-center justify-content-center">
+            <Pagination>{items}</Pagination>
+            <br />
+          </div>
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>
+                <p>User: {clickedUser?.id}</p>
+                <p>
+                  {clickedUser?.name} {clickedUser?.surname}
+                </p>
+                <p>{clickedUser?.email}</p>
+              </Modal.Title>
+            </Modal.Header>
+            {clickedUser?.role_id === 1 && (
+              <Modal.Footer className=" justify-content-center">
+                <Button
+                  variant="danger"
+                  onClick={() => {
+                    deleteButtonHandler(clickedUser);
+                  }}
+                >
+                  Delete
+                </Button>
+                <Button
+                  variant="primary"
+                  onClick={() => {
+                    upgradeButtonHandler(clickedUser);
+                  }}
+                >
+                  Upgrade
+                </Button>
+              </Modal.Footer>
+            )}
+          </Modal>
+        </>
+      )}
     </>
   );
 }
