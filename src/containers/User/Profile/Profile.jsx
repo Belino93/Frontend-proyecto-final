bce5d2c1aa28064d9755a71d99bff47679b3cf1b
belino93 <abelmadrid93@gmail.com>
2023-01-15 14:12:21 +0100

    responsinve profile input

--- src/containers/User/Profile/Profile.jsx
+++ src/containers/User/Profile/Profile.jsx
@@ -56,7 +56,9 @@ function Profile() {
       .then((res) => {
         setSearch(res.data.data);
       })
-      .catch((error) => {return});
+      .catch((error) => {
+        return;
+      });
   }, 500);
 
   const clickHandler = (repair) => {
@@ -74,97 +76,75 @@ function Profile() {
 
   return (
     <>
-    <Tabs
-          defaultActiveKey="repairs"
-          variant="pills"
-          onClick={(e) => profileClickHandler(e)}
-          className="tabs-container"
-        >
-          <Tab
-            key={"repair"}
-            id="repairs"
-            eventKey="repairs"
-            title="Repairs"
-            tabClassName="profile-menu"
-          ></Tab>
-          <Tab
-            key={"profile"}
-            id="repairs"
-            eventKey="profile"
-            title="Profile"
-            tabClassName="profile-menu"
-          ></Tab>
-        </Tabs>
-    <Container fluid className="min-vh-100 text-center container-home d-flex flex-column align-items-center justify-content-center">
-      {profile && (
-        <>
-          <ProfileCard className="card profile-card" userData={userPofile}></ProfileCard>
-        </>        
-      )}
-      {!profile && (
-        <Row className="m-2 p-1">
-              <SearchInput handler={inputHandler} />
+      <Container
+        fluid
+        className="min-vh-100 text-center container-home d-flex flex-column align-items-center justify-content-center"
+      >
+        <Row>
+          <Tabs
+            defaultActiveKey="repairs"
+            variant="pills"
+            onClick={(e) => profileClickHandler(e)}
+            className="tabs-container"
+          >
+            <Tab
+              key={"repair"}
+              id="repairs"
+              eventKey="repairs"
+              title="Repairs"
+              tabClassName="profile-menu"
+            ></Tab>
+            <Tab
+              key={"profile"}
+              id="repairs"
+              eventKey="profile"
+              title="Profile"
+              tabClassName="profile-menu"
+            ></Tab>
+          </Tabs>
         </Row>
-      )}
+        {profile && (
+          <>
+            <ProfileCard
+              className="card profile-card"
+              userData={userPofile}
+            ></ProfileCard>
+          </>
+        )}
+        {!profile && (
 
-      {userRepairs.length === 0 && search.length === 0 && !profile && (
-        <Row className="flex-grow-1">
-          <Col>
-            {repairError === "" && (
-              <div>
-                <Spinner
-                  animation="border"
-                  variant="light"
-                  className="spinner-load"
-                />
-              </div>
-            )}
-            {repairError !== "" && (
-              <div>
-                <h1>{repairError}</h1>
-              </div>
-            )}
-          </Col>
-        </Row>
-      )}
-      {search.length > 0 && !profile && (
-        <>
-          <Row className="">
-            {search.map((repair, index) => {
-              return (
-                <Col key={index} className="col-12 col-md-6">
-                  <div
-                    className="card"
-                    onClick={() => {
-                      clickHandler(repair);
-                    }}
-                  >
-                    <p>Repair No.{repair.id}</p>
-                    <p>
-                      {repair.brand} {repair.model}
-                    </p>
-                    <p>
-                      Repair type:{" "}
-                      <span className="highlighted">{repair.type}</span>
-                    </p>
-                    <p>
-                      Imei: <span className="highlighted">{repair.imei}</span>
-                    </p>
-                  </div>
-                </Col>
-              );
-            })}
+          <Row className="m-2">
+            <SearchInput handler={inputHandler} />
           </Row>
-        </>
-      )}
 
-      {search.length < 1 && !profile && (
-        <>
-          <Row className="">
-            {userRepairs.map((repair, index) => {
-              return (
-                <>
-                  <Col key={index} className="col-12 col-md-6 mb-2">
+        )}
+
+        {userRepairs.length === 0 && search.length === 0 && !profile && (
+          <Row className="flex-grow-1">
+            <Col>
+              {repairError === "" && (
+                <div>
+                  <Spinner
+                    animation="border"
+                    variant="light"
+                    className="spinner-load"
+                  />
+                </div>
+              )}
+              {repairError !== "" && (
+                <div>
+                  <h1>{repairError}</h1>
+                </div>
+              )}
+            </Col>
+          </Row>
+        )}
+        {search.length > 0 && !profile && (
+          <>
+            <Row className="">
+              {search.map((repair, index) => {
+                return (
+                  <Col key={index} className="col-12 col-md-6">
                     <div
                       className="card"
                       onClick={() => {
@@ -184,37 +164,70 @@ function Profile() {
                       </p>
                     </div>
                   </Col>
-                </>
-              );
-            })}
-          </Row>
-        </>
-      )}
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-        </Modal.Header>
+                );
+              })}
+            </Row>
+          </>
+        )}
+
+        {search.length < 1 && !profile && (
+          <>
+            <Row className="">
+              {userRepairs.map((repair, index) => {
+                return (
+                  <>
+                    <Col key={index} className="col-12 col-md-6 mb-2">
+                      <div
+                        className="card"
+                        onClick={() => {
+                          clickHandler(repair);
+                        }}
+                      >
+                        <p>Repair No.{repair.id}</p>
+                        <p>
+                          {repair.brand} {repair.model}
+                        </p>
+                        <p>
+                          Repair type:{" "}
+                          <span className="highlighted">{repair.type}</span>
+                        </p>
+                        <p>
+                          Imei:{" "}
+                          <span className="highlighted">{repair.imei}</span>
+                        </p>
+                      </div>
+                    </Col>
+                  </>
+                );
+              })}
+            </Row>
+          </>
+        )}
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton></Modal.Header>
           <Modal.Title className="d-flex flex-column justify-content-center align-items-center">
             <p>Repair No.{clickedRepair?.id}</p>
             <p>
               {clickedRepair?.brand} {clickedRepair?.model}
             </p>
           </Modal.Title>
-        <Modal.Body className="d-flex flex-column justify-content-center align-items-center">
-          <p>Repair type: {clickedRepair?.type}</p>
-          <p>IMEI: {clickedRepair?.imei}</p>
-          <p>Created: {clickedRepair?.created_at?.slice(0, 10)}</p>
-          <p>
-            Status: <span className="highlighted">{clickedRepair?.status}</span>
-          </p>
-          <p>Last status update: {clickedRepair?.updated_at?.slice(0, 10)}</p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
-    </Container>
+          <Modal.Body className="d-flex flex-column justify-content-center align-items-center">
+            <p>Repair type: {clickedRepair?.type}</p>
+            <p>IMEI: {clickedRepair?.imei}</p>
+            <p>Created: {clickedRepair?.created_at?.slice(0, 10)}</p>
+            <p>
+              Status:{" "}
+              <span className="highlighted">{clickedRepair?.status}</span>
+            </p>
+            <p>Last status update: {clickedRepair?.updated_at?.slice(0, 10)}</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      </Container>
     </>
   );
 }
